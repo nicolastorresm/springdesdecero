1a97d8fb71467e53ad9d388df0e6eb835fb68be5
Nicolas Torres <lsc.tomni@gmail.com>
2018-11-15 16:59:49 -0600

    Se implemento el DAO con la Interface CrudRepository

--- src/main/java/com/simms/springboot/app/models/service/ClienteServiceImpl.java
+++ src/main/java/com/simms/springboot/app/models/service/ClienteServiceImpl.java
@@ -18,7 +18,7 @@ public class ClienteServiceImpl implements IClienteService {
 	@Transactional(readOnly=true)
 	public List<Cliente> findAll() {
 		// TODO Auto-generated method stub
-		return clienteDao.findAll();
+		return (List<Cliente>)clienteDao.findAll();
 	}
 
 	@Override
@@ -33,14 +33,14 @@ public class ClienteServiceImpl implements IClienteService {
 	@Transactional(readOnly=true)
 	public Cliente findOne(Long id) {
 		// TODO Auto-generated method stub
-		return clienteDao.findOne(id);
+		return clienteDao.findById(id).orElse(null);
 	}
 
 	@Override
 	@Transactional
 	public void delete(Long id) {
 		// TODO Auto-generated method stub
-		clienteDao.delete(id);
+		clienteDao.deleteById(id);
 	}
 	
 	
